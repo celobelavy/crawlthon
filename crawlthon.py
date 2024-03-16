43be5f7de2223cd5fded808cb087bf2f3d480990
hamnahyun <hamnahyun@nyahyun.com>
2024-03-17 00:35:51 +0900

    telegram bot init

---
+++ crawlthon.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+def main():
+    load_dotenv()
+    BOT_TOKEN = os.environ.get("TelegramBotAPI")
+    
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
+
+if __name__ == "__main__" :
+    main()
